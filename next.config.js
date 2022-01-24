8352f63f98c4440925adfb46149ad7d503c80ccd
iketown <be@ike.works>
2022-01-24 14:16:36 -0600

    config changes 2:16

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  }
 }
 
 module.exports = nextConfig
